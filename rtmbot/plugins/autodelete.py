dad31743a4edff4bb3565ad60a07e0024beabb93
Tylor Garrett <tylorgarrett@gmail.com>
2017-08-07 19:57:47 -0400

    added autodelete

---
+++ rtmbot/plugins/autodelete.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from rtmbot.core import Plugin
+import json
+import re
+import requests
+import pyrebase
+from secrets import secrets
+
+firebaseConfig = secrets["firebase"]
+
+fire = pyrebase.initialize_app(firebaseConfig)
+
+class AutoDelete(Plugin):
+  
+    def process_message(self, msg):
+      text = msg.get("text", "")
+      channel = msg.get("channel", "");
+      user = msg.get("user", "");
+      ts = msg.get("ts", "")
+      thread_ts = msg.get("thread_ts", None)
+      subtype = msg.get("subtype", None)
+      if self.getChannel() == channel and self.getPermission() and thread_ts is None and subtype is None:
+        print(msg)
+        self.deleteMessage(channel, ts)
+      return
+
+    def output(self, channel, message):
+      self.outputs.append([channel, message])
+      return
+
+    def deleteMessage(self, channel, ts):
+      print(channel)
+      print(ts)
+
+    def getChannel(self):
+      db = fire.database()
+      results = db.child('config/announceChannel').get()
+      return results.val()
+
+    def getPermission(self):
+      db = fire.database()
+      results = db.child('config/autodelete').get()
+      return results.val()
+      
+
