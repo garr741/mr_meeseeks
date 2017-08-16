c20ed6fd7f06da6d5f5eb0bafb24266bab7adcaf
Tylor Garrett <tylorgarrett@gmail.com>
2017-08-15 23:13:48 -0400

    updated template

--- rtmbot/plugins/template.py
+++ rtmbot/plugins/template.py
@@ -4,17 +4,28 @@ from rtmbot.core import Plugin
 import json
 import re
 import requests
+import pyrebase
+from secrets import secrets
+
+firebaseConfig = secrets["firebase"]
+
+fire = pyrebase.initialize_app(firebaseConfig)
 
 class Template(Plugin):
   
     def process_message(self, msg):
       text = msg.get("text", "")
       channel = msg.get("channel", "");
-      match = re.findall(r"!do( .*)?", text)
-      if not match:
+      match = re.findall(r"!command( .*)?", text)
+      if not match or self.getPermission() is False:
         return
       return
 
     def output(self, channel, message):
-      #self.outputs.append([channel, message])
+      self.outputs.append([channel, message])
       return
+
+    def getPermission(self):
+      db = fire.database()
+      results = db.child('config/command').get()
+      return results.val()
