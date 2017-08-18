b3bb4b26266b4bcae17f57221cd7f6c974b8de54
Tylor Garrett <tylorgarrett@gmail.com>
2017-08-18 19:38:26 -0400

    added autodelete functions

--- rtmbot/plugins/autodelete.py
+++ rtmbot/plugins/autodelete.py
@@ -21,25 +21,30 @@ class AutoDelete(Plugin):
       thread_ts = msg.get("thread_ts", None)
       subtype = msg.get("subtype", None)
       if self.getChannel() == channel and self.getPermission() and thread_ts is None and subtype is None:
-        self.deleteMessage(channel, ts)
-      return
+        self.deleteMessage(channel, ts, user, text)
+      return 
 
-    def output(self, channel, message):
-      self.outputs.append([channel, message])
-      return
-
-    def deleteMessage(self, channel, ts):
+    def deleteMessage(self, channel, ts, user, text):
       data = {"channel": channel, 'ts': ts}
       results = requests.post('https://us-central1-glados-59d2e.cloudfunctions.net/deleteMessage', data=data)
+      self.notifyUser(user, text)
 
     def getChannel(self):
       db = fire.database()
-      results = db.child('config/announceChannel').get()
+      results = db.child('config/autodeleteChannel').get()
       return results.val()
 
     def getPermission(self):
       db = fire.database()
       results = db.child('config/autodelete').get()
       return results.val()
+
+    def notifyUser(self, user, text):
+      message = "Hi, your message was autodeleted. Posting in #announcements is not allowed.\n\n"
+      message = message + "Please use the slash command `/announce help` for more information about making an announcement or "
+      message = message + "reply using a Thread for follow-up questions/comments\n\n\n"
+      message = message + "> " + text
+      result = self.slack_client.api_call("chat.postMessage", channel=user, text=message)
+
       
 
